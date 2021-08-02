178ea9c237bd0cb999d4460973fa7b4a8e250b39
kehinde.adebayo <kehinde.adebayo@ektaco.ee>
2021-08-02 09:35:41 +0300

    Add automapper with pokemon profile

---
+++ Pokedex.Api/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Pokedex.Core.AutoMapperProfiles;
+
+namespace Pokedex.Api.Configurations
+{
+    /// <summary>
+    /// Mapper registration configs
+    /// </summary>
+    public static class AutoMapperConfiguration
+    {
+        /// <summary>
+        /// Extension method to register AutoMapper with specified configuration
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddAutoMapperConfiguration(this IServiceCollection services)
+        {
+            var mappingConfig = GetConfiguration();
+
+            var mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        private static MapperConfiguration GetConfiguration()
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new PokemonProfile());
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ Pokedex.Core/AutoMapperProfiles/PokemonProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Pokedex.Core.Models;
+using System.Linq;
+
+namespace Pokedex.Core.AutoMapperProfiles {
+    /// <summary>
+    /// Basic pokemon profile
+    /// </summary>
+    public class PokemonProfile : Profile {
+        public PokemonProfile()
+        {
+            CreateMap<Pokemon, PokemonDto>()
+                .ForMember(d => d.IsLegendary, map => map.MapFrom(h => h.Is_Legendary))
+                .ForMember(d => d.Habitat, map => map.MapFrom(h => h.Habitat.Name))
+                .ForMember(d => d.Description, m => m.MapFrom(h => h.Flavor_Text_Entries.FirstOrDefault().Flavor_Text));
+        }
+    }
+}
