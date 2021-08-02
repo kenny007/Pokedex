e10a1cfdb3ffe5bd914c35753571c6ef3b80a6c7
kehinde.adebayo <kehinde.adebayo@ektaco.ee>
2021-08-02 09:33:09 +0300

    Add GET pokemon models

--- Pokedex.Core/Models/Pokemon.cs
+++ Pokedex.Core/Models/Pokemon.cs
@@ -1,9 +1,20 @@
-﻿namespace Pokedex.Core.Models {
+﻿using System.Collections.Generic;
+
+namespace Pokedex.Core.Models {
     public class Pokemon {
         public string Name { get; set; }
+        public bool Is_Legendary { get; set; }
 
-        public string  Description { get; set; }
-        public string Habitat { get; set; }
-        public bool IsLegendary { get; set; }
+        public Habitat Habitat { get; set; }
+        public IEnumerable<FlavorTextEntries> Flavor_Text_Entries { get; set; }
+    }
+
+    public class Habitat {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class FlavorTextEntries {
+        public string Flavor_Text { get; set; }
     }
 }
---
+++ Pokedex.Core/Models/PokemonDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Pokedex.Core.Models {
+    public class PokemonDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public bool IsLegendary { get; set; }
+
+        public string Habitat { get; set; }
+    }
+}
